613e7aac59e4b5472ef17fa98cf10b39b510f0a0
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2024-05-12 18:10:55 +0200

    build: add project configuration

---
+++ src/antares_performance_tests/__main__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2024-present Laurent LAPORTE <laurent.laporte.pro@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+import sys
+
+if __name__ == "__main__":
+    from antares_performance_tests.cli import antares_performance_tests
+
+    sys.exit(antares_performance_tests())
